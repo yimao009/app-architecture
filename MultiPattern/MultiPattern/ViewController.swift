5354200e5d82e547e5a7568be8b2d496a8aec83e
global_grz <13331135676@163.com>
2021-04-21 21:52:01 +0800

    Add MVC architecture

--- MultiPattern/MultiPattern/ViewController.swift
+++ MultiPattern/MultiPattern/ViewController.swift
@@ -10,42 +10,73 @@ import SnapKit
 
 class ViewController: UIViewController {
 
+    let model = Model(value: "initial value")
+
+    @IBOutlet weak var mvcTextField: UITextField!
+    
+    @IBOutlet weak var mvcButton: UIButton!
+
+    // Strong references
+    var mvcObserver: NSObjectProtocol?
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
 
-//        let label = UILabel()
-//        label.text = "UILabel"
-//        view.addSubview(label)
-//
-//        let testView = UIView()
-//        testView.backgroundColor = UIColor.cyan
-//        view.addSubview(testView)
-//
-//        let container = UILayoutGuide()
-//        view.addLayoutGuide(container)
-//
-//        label.snp.makeConstraints { (make) in
-//            make.left.equalTo(container)
-//            make.centerY.equalTo(container)
-//        }
-//
-//        testView.snp.makeConstraints { (make) in
-//            make.left.equalTo(label.snp.right).offset(20)
-//            make.height.equalTo(40)
-//            make.width.equalTo(120)
-//            make.right.equalTo(container)
-//            make.centerY.equalTo(container)
-//        }
-//
-//        container.snp.makeConstraints { (make) in
-//            make.height.equalTo(5)
-//            make.center.equalTo(view)
-//        }
+        mvcDidLoad()
+
     }
 
 //    override func viewDidLayoutSubviews() {
 //        super.viewDidLayoutSubviews()
 //    }
+
 }
 
+extension ViewController {
+
+    func mvcDidLoad() {
+        mvcTextField.text = model.value
+        mvcObserver = NotificationCenter.default.addObserver(forName: Model.textDidChange, object: nil, queue: nil) { [mvcTextField] (notif) in
+            mvcTextField?.text = notif.userInfo?[Model.textKey] as? String
+        }
+    }
+
+    @IBAction func mvcButtonPressed() {
+        model.value = mvcTextField?.text ?? "non"
+    }
+}
+
+
+
+extension ViewController {
+    func recoard() {
+        //        let label = UILabel()
+        //        label.text = "UILabel"
+        //        view.addSubview(label)
+        //
+        //        let testView = UIView()
+        //        testView.backgroundColor = UIColor.cyan
+        //        view.addSubview(testView)
+        //
+        //        let container = UILayoutGuide()
+        //        view.addLayoutGuide(container)
+        //
+        //        label.snp.makeConstraints { (make) in
+        //            make.left.equalTo(container)
+        //            make.centerY.equalTo(container)
+        //        }
+        //
+        //        testView.snp.makeConstraints { (make) in
+        //            make.left.equalTo(label.snp.right).offset(20)
+        //            make.height.equalTo(40)
+        //            make.width.equalTo(120)
+        //            make.right.equalTo(container)
+        //            make.centerY.equalTo(container)
+        //        }
+        //
+        //        container.snp.makeConstraints { (make) in
+        //            make.height.equalTo(5)
+        //            make.center.equalTo(view)
+        //        }
+    }
+}
