5850da1472fcd710af5a93bfc196497618d4b3ce
global_grz <13331135676@163.com>
2021-04-21 15:12:39 +0800

    Add StackView in ScrollView for StoryBoard

--- MultiPattern/MultiPattern/ViewController.swift
+++ MultiPattern/MultiPattern/ViewController.swift
@@ -6,14 +6,46 @@
 //
 
 import UIKit
+import SnapKit
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+
+//        let label = UILabel()
+//        label.text = "UILabel"
+//        view.addSubview(label)
+//
+//        let testView = UIView()
+//        testView.backgroundColor = UIColor.cyan
+//        view.addSubview(testView)
+//
+//        let container = UILayoutGuide()
+//        view.addLayoutGuide(container)
+//
+//        label.snp.makeConstraints { (make) in
+//            make.left.equalTo(container)
+//            make.centerY.equalTo(container)
+//        }
+//
+//        testView.snp.makeConstraints { (make) in
+//            make.left.equalTo(label.snp.right).offset(20)
+//            make.height.equalTo(40)
+//            make.width.equalTo(120)
+//            make.right.equalTo(container)
+//            make.centerY.equalTo(container)
+//        }
+//
+//        container.snp.makeConstraints { (make) in
+//            make.height.equalTo(5)
+//            make.center.equalTo(view)
+//        }
     }
 
-
+//    override func viewDidLayoutSubviews() {
+//        super.viewDidLayoutSubviews()
+//    }
 }
 
